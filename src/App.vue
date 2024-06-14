eb1d5c4029bc1dab99281a22b20265da9f6ac9ba
DenLopes <denis.gnl@gmail.com>
2024-06-13 23:59:40 -0300

    using pixi js

--- src/App.vue
+++ src/App.vue
@@ -1,186 +1,22 @@
 <template>
-  <div class="flex md:justify-center w-screen mb-1">
-    <div class="flex lg:flex-row md:justify-around flex-col w-full m-2">
-      <div class="flex self-center w-fit rounded-md bg-red-400 mb-1 lg:m-0">
-        <button @click="randomGrid()" class="h-10 w-fit p-2 font-bold">RANDOM</button>
-        <button @click="clearGrid()" class="h-10 w-fit p-2 px-4 font-bold bg-yellow-400">
-          CLEAR
+  <div class="absolute">
+    <div class="flex md:flex-row flex-col items-center pt-4">
+      <p class="flex items-center mr-1 font-bold text-white">TILES SIZE:</p>
+      <div class="rounded-md h-fit bg-blue-400 mr-2">
+        <button @click="pixelSize++" class="font-extrabold h-7 w-10 m-auto">+</button>
+        <button @click="pixelSize--" class="font-extrabold h-7 w-10 m-auto border-l-2 border-black">
+          -
         </button>
-        <button
-          @click="running = !running"
-          class="h-10 w-fit p-2 px-4 font-bold rounded-r-md bg-green-400"
-        >
-          {{ running ? 'STOP' : 'START' }}
-        </button>
-      </div>
-      <div class="flex justify-center">
-        <div class="flex md:flex-row flex-col items-center">
-          <p class="flex items-center mr-1 font-bold">COLUMNS:</p>
-          <div class="rounded-md h-fit bg-blue-400 mr-2">
-            <button @click="columns++" class="font-extrabold h-7 w-10 m-auto">+</button>
-            <input
-              type="text"
-              class="font-extrabold h-8 w-12 text-center m-auto border-l-2 border-black"
-              v-model="columns"
-            />
-            <button
-              @click="columns--"
-              class="font-extrabold h-7 w-10 m-auto border-l-2 border-black"
-            >
-              -
-            </button>
-          </div>
-        </div>
-        <div class="flex md:flex-row flex-col items-center">
-          <p class="flex items-center mr-1 font-bold">ROWNS:</p>
-          <div class="rounded-md h-fit bg-blue-400 mr-2">
-            <button @click="rows++" class="font-extrabold h-7 w-10 m-auto">+</button>
-            <input
-              type="text"
-              class="font-extrabold h-8 w-12 text-center m-auto border-l-2 border-black"
-              v-model="rows"
-            />
-            <button @click="rows--" class="font-extrabold h-7 w-10 m-auto border-l-2 border-black">
-              -
-            </button>
-          </div>
-        </div>
-        <div class="flex md:flex-row flex-col items-center">
-          <p class="flex items-center mr-1 font-bold">TILES SIZE:</p>
-          <div class="rounded-md h-fit bg-blue-400 mr-2">
-            <button @click="pixelSize++" class="font-extrabold h-7 w-10 m-auto">+</button>
-            <input
-              type="text"
-              class="font-extrabold h-8 w-12 text-center m-auto border-l-2 border-black"
-              v-model="pixelSize"
-            />
-            <button
-              @click="pixelSize--"
-              class="font-extrabold h-7 w-10 m-auto border-l-2 border-black"
-            >
-              -
-            </button>
-          </div>
-        </div>
-        <div class="flex md:flex-row flex-col items-center">
-          <p class="flex items-center mr-1 font-bold">SPEED:</p>
-          <div class="rounded-md h-fit bg-blue-400 mr-2">
-            <button @click="simulationSpeed++" class="font-extrabold h-7 w-10 m-auto">+</button>
-            <input
-              type="text"
-              class="font-extrabold h-8 w-12 text-center m-auto border-l-2 border-black"
-              v-model="simulationSpeed"
-            />
-            <button
-              @click="simulationSpeed--"
-              class="font-extrabold h-7 w-10 m-auto border-l-2 border-black"
-            >
-              -
-            </button>
-          </div>
-        </div>
       </div>
     </div>
+
   </div>
-  <div class="flex justify-center overflow-auto">
-    <Grid
-      ref="gridRef"
-      :pixelSize="pixelSize"
-      :simulationSpeed="simulationSpeed"
-      :running="running"
-      :columns="debounceColumns"
-      :rows="debounceRows"
-    ></Grid>
-  </div>
+  <Grid :scale="pixelSize" />
 </template>
 <script setup>
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
 import Grid from './components/Grid.vue'
 
-const running = ref(false)
-const pixelSize = ref(16)
-const gridRef = ref(null)
-const simulationSpeed = ref(100)
-const columns = ref(400)
-const debounceColumns = ref(400)
-const rows = ref(300)
-const debounceRows = ref(300)
+const pixelSize = ref(10)
 
-watch(columns, async () => {
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve()
-    }, 250)
-  })
-  if (columns.value < 10) {
-    columns.value = 10
-  }
-  debounceColumns.value = columns.value
-})
-
-watch(rows, async () => {
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve()
-    }, 250)
-  })
-  if (rows.value < 10) {
-    rows.value = 10
-  }
-  debounceRows.value = rows.value
-})
-
-watch(debounceColumns, (newValue, oldValue) => {
-  const valueToBeChanged = Math.abs(newValue - oldValue)
-  if (valueToBeChanged > 0) {
-    if (newValue > oldValue) {
-      moreColumns(valueToBeChanged)
-    } else {
-      lessColumns(valueToBeChanged)
-    }
-  }
-})
-
-watch(debounceRows, (newValue, oldValue) => {
-  const valueToBeChanged = Math.abs(newValue - oldValue)
-  if (valueToBeChanged > 0) {
-    if (newValue > oldValue) {
-      moreRows(valueToBeChanged)
-    } else {
-      lessRows(valueToBeChanged)
-    }
-  }
-})
-
-const randomGrid = () => {
-  gridRef.value.randomGrid()
-}
-
-const clearGrid = () => {
-  gridRef.value.clearGrid()
-}
-
-const moreColumns = (amount) => {
-  for (let i = 0; i < amount; i++) {
-    gridRef.value.moreColumns()
-  }
-}
-
-const lessColumns = (amount) => {
-  for (let i = 0; i < amount; i++) {
-    gridRef.value.lessColumns()
-  }
-}
-
-const moreRows = (amount) => {
-  for (let i = 0; i < amount; i++) {
-    gridRef.value.moreRows()
-  }
-}
-
-const lessRows = (amount) => {
-  for (let i = 0; i < amount; i++) {
-    gridRef.value.lessRows()
-  }
-}
 </script>
--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,160 +1,73 @@
 <template>
-  <div class="flex justify-center items-center w-full h-full">
-    <canvas ref="canvasRef" :width="(columns + 1) * pixelSize" :height="(rows + 1) * pixelSize"></canvas>
-  </div>
+  <div ref="pixiContainer"></div>
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted } from 'vue'
+import { ref, onMounted, onUnmounted } from 'vue';
+import { Application, Sprite, Texture, Container } from 'pixi.js';
 
 const props = defineProps({
-  pixelSize: Number,
-  running: Boolean,
-  simulationSpeed: Number,
-  columns: Number,
-  rows: Number
-})
+  scale: Number,
+});
+const pixiContainer = ref(null);
 
-//reactive values
-const ctx = ref()
-const canvasRef = ref()
+const app = new Application();
+const container = new Container();
 
-// variables
-let grid = Array.from({ length: props.columns + 1 }, () =>
-  Array.from({ length: props.rows + 1 }, () => (false))
-)
-let cellsAlive = Array.from({ length: props.columns * props.rows }, () => ({
-}))
-let mouseDown = false
+const pixelSize = 1
+let width = 0;
+let height = 0;
 
-const changeGrid = (e) => {
-  const x = Math.floor(e.offsetX / props.pixelSize)
-  const y = Math.floor(e.offsetY / props.pixelSize)
-  if (x > 0 && x < props.columns && y > 0 && y < props.rows) {
-    grid[x][y] = !grid[x][y]
-    if (grid[x][y]) {
-      cellsAlive.push({ x, y })
+const getCellFromPool = (x, y) => {
+  const square = new Sprite(Texture.WHITE);
+  square.width = pixelSize;
+  square.height = pixelSize;
+  square.tint = 0xffffff;
+  square.position.set(x * pixelSize, y * pixelSize);
+  return square;
+};
+
+const renderCells = (newCells) => {
+  container.removeChildren();
+  for (let i = 0; i < newCells.length; i++) {
+    container.addChild(getCellFromPool(newCells[i].x, newCells[i].y));
+  }
+  container.scale.set(props.scale, props.scale);
+  container.position.set(app.screen.width / 2 - ((width * pixelSize) * props.scale) / 2, app.screen.height / 2 - ((height * pixelSize) * props.scale) / 2);
+};
+
+onMounted(async () => {
+  await app.init({
+    resizeTo: window,
+    backgroundColor: 0x000000,
+  });
+  pixiContainer.value.appendChild(app.canvas);
+
+  app.stage.addChild(container);
+  container.scale.set(props.scale, props.scale);
+
+  const socket = new WebSocket('ws://localhost:8080/ws');
+
+  socket.onopen = () => {
+    console.log('Connected to server');
+  };
+
+  let firstMessage = false;
+  socket.onmessage = (e) => {
+    const data = JSON.parse(e.data);
+    if (!firstMessage) {
+      console.log(`Grid size: ${data.width} x ${data.height}`);
+      width = data.width;
+      height = data.height;
+      container.position.set(app.screen.width / 2 - ((width * pixelSize) * props.scale) / 2, app.screen.height / 2 - ((height * pixelSize) * props.scale) / 2);
+      firstMessage = true;
     } else {
-      cellsAlive = cellsAlive.filter(cell => !(cell.x === x && cell.y === y));
-    }
-  }
-}
-
-const randomGrid = () => {
-  cellsAlive = []
-  grid = Array.from({ length: props.columns + 1 }, () =>
-    Array.from({ length: props.rows + 1 }, () => (false))
-  )
-  for (let i = 1; i < props.columns; i++) {
-    for (let j = 1; j < props.rows; j++) {
-      if (Math.random() < 0.4) {
-        cellsAlive.push({ x: i, y: j })
-        grid[i][j] = true
-      }
-    }
-  }
-}
-
-const clearGrid = () => {
-  cellsAlive = []
-  grid = Array.from({ length: props.columns + 1 }, () =>
-    Array.from({ length: props.rows + 1 }, () => (false))
-  )
-}
-
-const neighborOffsets = [
-  [-1, -1],
-  [-1, 0],
-  [-1, 1],
-  [0, -1],
-  [0, 1],
-  [1, -1],
-  [1, 0],
-  [1, 1]
-]
-
-const applyRules = async () => {
-  console.time('applyRules')
-  const newGrid = Array.from({ length: props.columns + 1 }, () =>
-    Array.from({ length: props.rows + 1 }, () => (false))
-  )
-  const newCellsAlive = []
-  for (let i = 1; i < props.columns; i++) {
-    for (let j = 1; j < props.rows; j++) {
-      let neighbors = 0
-      for (const [x, y] of neighborOffsets) {
-        if (grid[i + x] && grid[i + x][j + y]) {
-          neighbors++
-        }
-      }
-      if (grid[i][j]) {
-        if (neighbors === 2 || neighbors === 3) {
-          newGrid[i][j] = true
-          newCellsAlive.push({ x: i, y: j })
-        }
-      } else {
-        if (neighbors === 3) {
-          newGrid[i][j] = true
-          newCellsAlive.push({ x: i, y: j })
-        }
-      }
-    }
-  }
-  grid = newGrid
-  cellsAlive = newCellsAlive
-  console.timeEnd('applyRules')
-}
-
-const renderGrid = async () => {
-  ctx.value.clearRect(0, 0, canvasRef.value.width, canvasRef.value.height)
-  for (const { x, y } of cellsAlive) {
-    ctx.value.fillRect(x * props.pixelSize, y * props.pixelSize, props.pixelSize, props.pixelSize)
-  }
-
-  setTimeout(() => {
-    renderGrid()
-    if (props.running) {
-      applyRules()
-    }
-  }, props.simulationSpeed)
-}
-
-
-
-defineExpose({
-  randomGrid,
-  clearGrid,
-})
-
-onMounted(() => {
-  ctx.value = canvasRef.value.getContext('2d');
-  ctx.value.fillStyle = 'black';
-  renderGrid();
-
-  const handleMouseDown = (e) => {
-    mouseDown = true;
-    changeGrid(e);
-  };
-
-  const handleMouseUp = () => {
-    mouseDown = false;
-  };
-
-  const handleMouseMove = (e) => {
-    if (mouseDown) {
-      changeGrid(e);
+      renderCells(data)
     }
   };
-
-  canvasRef.value.addEventListener('mousedown', handleMouseDown);
-  canvasRef.value.addEventListener('mouseup', handleMouseUp);
-  canvasRef.value.addEventListener('mousemove', handleMouseMove);
 });
 
-// Existing onUnmounted function with updates to remove the correct listeners
 onUnmounted(() => {
-  canvasRef.value.removeEventListener('mousedown', handleMouseDown);
-  canvasRef.value.removeEventListener('mouseup', handleMouseUp);
-  canvasRef.value.removeEventListener('mousemove', handleMouseMove);
+  app.destroy();
 });
 </script>
