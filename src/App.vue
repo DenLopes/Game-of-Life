6ea15b2b600d6b543e13ec1450d0712d1c43b239
DenLopes <denis.gnl@gmail.com>
2024-06-15 23:21:35 -0300

    better rendering, interaction and fixes

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,85 @@
 <template>
-  <div class="absolute">
-    <div class="flex md:flex-row flex-col items-center pt-4">
-      <p class="flex items-center mr-1 font-bold text-white">TILES SIZE:</p>
-      <div class="rounded-md h-fit bg-blue-400 mr-2">
-        <button @click="pixelSize++" class="font-extrabold h-7 w-10 m-auto">+</button>
-        <button @click="pixelSize--" class="font-extrabold h-7 w-10 m-auto border-l-2 border-black">
-          -
-        </button>
-      </div>
+  <div class="absolute flex gap-4 p-2">
+    <div class="flex justify-center">
+      <button @click="randomGrid"
+        class="bg-black text-white font-bold p-2 rounded-md border border-solid border-white">Random
+        Grid</button>
     </div>
-
+    <div class="flex justify-center">
+      <button @click="clearGrid"
+        class="bg-black text-white font-bold p-2 rounded-md border border-solid border-white">Clear
+        Grid</button>
+    </div>
+    <div class="flex justify-center">
+      <button @click="gameState" class="bg-black text-white font-bold p-2 rounded-md border border-solid border-white">{{
+        isGameRunning ? 'Stop Game' : 'Start Game'
+        }}</button>
+    </div>
   </div>
-  <Grid :scale="pixelSize" />
+  <Grid ref="gridRef" :scale="pixelSize" :game-running="isGameRunning"
+    @update:gameRunning="(event) => (isGameRunning = event)" />
 </template>
 <script setup>
-import { ref } from 'vue'
+import { onMounted, onUnmounted, ref } from 'vue'
 import Grid from './components/Grid.vue'
 
-const pixelSize = ref(10)
+const pixelSize = ref(1)
+const gridRef = ref(null)
+const isGameRunning = ref(true)
 
+const randomGrid = () => {
+  gridRef.value.randomGrid()
+}
+
+const clearGrid = () => {
+  gridRef.value.clearGrid()
+}
+
+const gameState = () => {
+  if (isGameRunning.value) {
+    gridRef.value.stopGame()
+  } else {
+    gridRef.value.startGame()
+  }
+}
+
+const lerp = (a, b, t) => {
+  return a + (b - a) * t;
+};
+onMounted(() => {
+  let animationFrameId;
+  let targetPixelSize = pixelSize.value;
+
+  const animatePixelSize = () => {
+    pixelSize.value = lerp(pixelSize.value, targetPixelSize, 0.2);
+    if (Math.abs(pixelSize.value - targetPixelSize) > 0.01) {
+      animationFrameId = requestAnimationFrame(animatePixelSize);
+    } else {
+      animationFrameId = null;
+    }
+  };
+
+  addEventListener('wheel', (e) => {
+    if (e.deltaY < 0) {
+      targetPixelSize = pixelSize.value + 0.1;
+      if (targetPixelSize > 2) {
+        targetPixelSize = 2;
+      }
+    } else {
+      targetPixelSize = pixelSize.value - 0.1;
+      if (targetPixelSize < 1) {
+        targetPixelSize = 1;
+      }
+    }
+
+    if (!animationFrameId) {
+      animationFrameId = requestAnimationFrame(animatePixelSize);
+    }
+  });
+});
+
+onUnmounted(() => {
+  removeEventListener('wheel');
+  cancelAnimationFrame(animationFrameId);
+});
 </script>
--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -4,37 +4,89 @@
 
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
-import { Application, Sprite, Texture, Container } from 'pixi.js';
+import { Application, Sprite, Texture, Container, Graphics, GraphicsContext, childrenHelperMixin } from 'pixi.js';
+
+const socket = new WebSocket(import.meta.env.VITE_WS_URL);
 
 const props = defineProps({
   scale: Number,
+  gameRunning: Boolean,
 });
+
+const emit = defineEmits(['update:gameRunning']);
+
 const pixiContainer = ref(null);
+
+socket.onopen = () => {
+  console.log('Connected to server');
+};
 
 const app = new Application();
 const container = new Container();
 
-const pixelSize = 1
+const pixelSize = 10;
 let width = 0;
 let height = 0;
+let cellsPool = [];
+const containerGrid = new Container();
 
-const getCellFromPool = (x, y) => {
+const createCell = (x, y) => {
   const square = new Sprite(Texture.WHITE);
-  square.width = pixelSize;
-  square.height = pixelSize;
+  square.width = pixelSize - 1;
+  square.height = pixelSize - 1;
   square.tint = 0xffffff;
-  square.position.set(x * pixelSize, y * pixelSize);
+  square.position.set(x * pixelSize + 1, y * pixelSize + 1);
+  cellsPool.push(square);
   return square;
 };
 
+const bg = new Sprite(Texture.WHITE);
+bg.tint = 0x000000;
+bg.alpha = 0;
+bg.position.set(0, 0);
+
 const renderCells = (newCells) => {
-  container.removeChildren();
-  for (let i = 0; i < newCells.length; i++) {
-    container.addChild(getCellFromPool(newCells[i].x, newCells[i].y));
-  }
+  cellsPool.forEach((cell) => {
+    container.removeChild(cell);
+    cell.destroy();
+  });
+
+  const centerX = app.screen.width / 2;
+  const centerY = app.screen.height / 2;
+  const scaledWidth = (width * pixelSize) * props.scale;
+  const scaledHeight = (height * pixelSize) * props.scale;
+
+  newCells.forEach(({ x, y }) => {
+    let cell = createCell(x, y);
+    cell.renderable = !(x < 0 || y < 0 || x > width * pixelSize || y > height * pixelSize);
+    container.addChild(cell);
+  });
+
+  containerGrid.children.forEach((line) => {
+    line.renderable = !(line.x < 0 || line.y < 0 || line.x > width * pixelSize || line.y > height * pixelSize);
+  });
+
   container.scale.set(props.scale, props.scale);
-  container.position.set(app.screen.width / 2 - ((width * pixelSize) * props.scale) / 2, app.screen.height / 2 - ((height * pixelSize) * props.scale) / 2);
+  container.position.set(centerX - scaledWidth / 2, centerY - scaledHeight / 2);
 };
+
+const randomGrid = () => {
+  socket.send('random');
+};
+
+const clearGrid = () => {
+  socket.send('clear');
+};
+
+const stopGame = () => {
+  socket.send('stop');
+};
+
+const startGame = () => {
+  socket.send('start');
+};
+
+defineExpose({ randomGrid, clearGrid, stopGame, startGame });
 
 onMounted(async () => {
   await app.init({
@@ -45,26 +97,62 @@ onMounted(async () => {
 
   app.stage.addChild(container);
   container.scale.set(props.scale, props.scale);
+  container.eventMode = 'static';
 
-  const socket = new WebSocket(import.meta.env.VITE_WS_URL);
-
-  socket.onopen = () => {
-    console.log('Connected to server');
-  };
-
-  let firstMessage = false;
   socket.onmessage = (e) => {
     const data = JSON.parse(e.data);
-    if (!firstMessage) {
-      console.log(`Grid size: ${data.width} x ${data.height}`);
-      width = data.width;
-      height = data.height;
+    if (height != data.gameState.height || width != data.gameState.width) {
+      width = data.gameState.width;
+      height = data.gameState.height;
+      for (let i = 0; i <= width; i++) {
+        const line = new Graphics().rect(i * pixelSize, 0, 1, height * pixelSize).fill({ color: 0xffffff, alpha: 0.25 });
+        containerGrid.addChild(line);
+      }
+      for (let i = 0; i <= height; i++) {
+        const line = new Graphics().rect(0, i * pixelSize, width * pixelSize, 1).fill({ color: 0xffffff, alpha: 0.25 });
+        containerGrid.addChild(line);
+      }
+      bg.width = width * pixelSize
+      bg.height = height * pixelSize
+      container.addChildAt(containerGrid, 0);
+      container.addChildAt(bg, 1);
       container.position.set(app.screen.width / 2 - ((width * pixelSize) * props.scale) / 2, app.screen.height / 2 - ((height * pixelSize) * props.scale) / 2);
-      firstMessage = true;
-    } else {
-      renderCells(data)
     }
+    if (data.gameState.playing) {
+      emit('update:gameRunning', true);
+    } else {
+      emit('update:gameRunning', false);
+    }
+    renderCells(data.cells);
   };
+
+  socket.onclose = () => {
+    console.log('Connection closed');
+  };
+
+  socket.onerror = (e) => {
+    console.error('Error:', e);
+  };
+
+  let isDragging = false;
+  container.on('pointerdown', (e) => {
+    isDragging = true;
+    const x = Math.floor((e.global.x - container.position.x) / (pixelSize * props.scale));
+    const y = Math.floor((e.global.y - container.position.y) / (pixelSize * props.scale));
+    socket.send(JSON.stringify({ "x": x, "y": y }));
+  });
+
+  container.on('pointerup', (e) => {
+    isDragging = false;
+  });
+
+  container.on('pointermove', (e) => {
+    if (isDragging) {
+      const x = Math.floor((e.global.x - container.position.x) / (pixelSize * props.scale));
+      const y = Math.floor((e.global.y - container.position.y) / (pixelSize * props.scale));
+      socket.send(JSON.stringify({ "x": x, "y": y }));
+    }
+  });
 });
 
 onUnmounted(() => {
