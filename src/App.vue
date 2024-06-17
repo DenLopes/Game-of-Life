c7e05662ceec98acd90eb50e32796787bf0406c3
DenLopes <denis.gnl@gmail.com>
2024-06-16 23:10:24 -0300

    better rendering and interactions

--- src/App.vue
+++ src/App.vue
@@ -11,19 +11,19 @@
         Grid</button>
     </div>
     <div class="flex justify-center">
-      <button @click="gameState" class="bg-black text-white font-bold p-2 rounded-md border border-solid border-white">{{
-        isGameRunning ? 'Stop Game' : 'Start Game'
+      <button @click="gameState"
+        class="bg-black text-white font-bold p-2 rounded-md border border-solid border-white">{{
+          isGameRunning ? 'Stop Game' : 'Start Game'
         }}</button>
     </div>
   </div>
-  <Grid ref="gridRef" :scale="pixelSize" :game-running="isGameRunning"
+  <Grid ref="gridRef" :game-running="isGameRunning"
     @update:gameRunning="(event) => (isGameRunning = event)" />
 </template>
 <script setup>
-import { onMounted, onUnmounted, ref } from 'vue'
+import { ref } from 'vue'
 import Grid from './components/Grid.vue'
 
-const pixelSize = ref(1)
 const gridRef = ref(null)
 const isGameRunning = ref(true)
 
@@ -42,44 +42,4 @@ const gameState = () => {
     gridRef.value.startGame()
   }
 }
-
-const lerp = (a, b, t) => {
-  return a + (b - a) * t;
-};
-onMounted(() => {
-  let animationFrameId;
-  let targetPixelSize = pixelSize.value;
-
-  const animatePixelSize = () => {
-    pixelSize.value = lerp(pixelSize.value, targetPixelSize, 0.2);
-    if (Math.abs(pixelSize.value - targetPixelSize) > 0.01) {
-      animationFrameId = requestAnimationFrame(animatePixelSize);
-    } else {
-      animationFrameId = null;
-    }
-  };
-
-  addEventListener('wheel', (e) => {
-    if (e.deltaY < 0) {
-      targetPixelSize = pixelSize.value + 0.1;
-      if (targetPixelSize > 2) {
-        targetPixelSize = 2;
-      }
-    } else {
-      targetPixelSize = pixelSize.value - 0.1;
-      if (targetPixelSize < 1) {
-        targetPixelSize = 1;
-      }
-    }
-
-    if (!animationFrameId) {
-      animationFrameId = requestAnimationFrame(animatePixelSize);
-    }
-  });
-});
-
-onUnmounted(() => {
-  removeEventListener('wheel');
-  cancelAnimationFrame(animationFrameId);
-});
 </script>
--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -4,7 +4,7 @@
 
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
-import { Application, Sprite, Texture, Container, Graphics, GraphicsContext, childrenHelperMixin } from 'pixi.js';
+import { Application, Sprite, Texture, Container, Graphics, GraphicsContext, childrenHelperMixin, Point } from 'pixi.js';
 
 const socket = new WebSocket(import.meta.env.VITE_WS_URL);
 
@@ -24,11 +24,15 @@ socket.onopen = () => {
 const app = new Application();
 const container = new Container();
 
+let gridScale = 4
 const pixelSize = 10;
 let width = 0;
 let height = 0;
-let cellsPool = [];
+let cellsPoolMap = new Map();
+let animationFrameId;
+let targetPixelSize = gridScale;
 const containerGrid = new Container();
+let containerPosition = { x: 0, y: 0 }
 
 const createCell = (x, y) => {
   const square = new Sprite(Texture.WHITE);
@@ -36,7 +40,6 @@ const createCell = (x, y) => {
   square.height = pixelSize - 1;
   square.tint = 0xffffff;
   square.position.set(x * pixelSize + 1, y * pixelSize + 1);
-  cellsPool.push(square);
   return square;
 };
 
@@ -45,29 +48,51 @@ bg.tint = 0x000000;
 bg.alpha = 0;
 bg.position.set(0, 0);
 
-const renderCells = (newCells) => {
-  cellsPool.forEach((cell) => {
-    container.removeChild(cell);
-    cell.destroy();
+const renderCells = () => {
+  const minX = container.x;
+  const minY = container.y;
+  const maxX = app.screen.width;
+  const maxY = app.screen.height;
+
+  for (const cell of container.children) {
+    if (cell.x + pixelSize < minX || cell.x > maxX || cell.y + pixelSize < minY || cell.y > maxY) {
+      cell.renderable = false;
+    }
+  }
+};
+
+const updateCells = (newCells) => {
+  const cellsToUpdate = new Map();
+
+  // Mark all existing cells for removal
+  cellsPoolMap.forEach((cell, id) => {
+    cellsToUpdate.set(id, { cell, action: 'remove' });
   });
 
-  const centerX = app.screen.width / 2;
-  const centerY = app.screen.height / 2;
-  const scaledWidth = (width * pixelSize) * props.scale;
-  const scaledHeight = (height * pixelSize) * props.scale;
-
+  // Process new cells, either marking them for addition or cancelling removal
   newCells.forEach(({ x, y }) => {
-    let cell = createCell(x, y);
-    cell.renderable = !(x < 0 || y < 0 || x > width * pixelSize || y > height * pixelSize);
-    container.addChild(cell);
+    const id = `${x}_${y}`;
+    if (cellsToUpdate.has(id)) {
+      // If already marked for removal, cancel the removal
+      cellsToUpdate.get(id).action = 'keep';
+    } else {
+      // Mark new cell for addition
+      const newCell = createCell(x, y);
+      cellsToUpdate.set(id, { cell: newCell, action: 'add' });
+    }
   });
 
-  containerGrid.children.forEach((line) => {
-    line.renderable = !(line.x < 0 || line.y < 0 || line.x > width * pixelSize || line.y > height * pixelSize);
+  // Execute the updates
+  cellsToUpdate.forEach(({ cell, action }, id) => {
+    if (action === 'add') {
+      container.addChild(cell);
+      cellsPoolMap.set(id, cell);
+    } else if (action === 'remove') {
+      container.removeChild(cell);
+      cell.destroy();
+      cellsPoolMap.delete(id);
+    }
   });
-
-  container.scale.set(props.scale, props.scale);
-  container.position.set(centerX - scaledWidth / 2, centerY - scaledHeight / 2);
 };
 
 const randomGrid = () => {
@@ -86,6 +111,24 @@ const startGame = () => {
   socket.send('start');
 };
 
+const lerp = (a, b, t) => {
+  return a + (b - a) * t;
+};
+
+const animateResize = () => {
+  let prevGridScale = gridScale;
+  gridScale = lerp(gridScale, targetPixelSize, 0.2);
+  if (Math.abs(gridScale - targetPixelSize) > 0.01) {
+    container.scale.set(gridScale, gridScale);
+    containerPosition.x += ((width * pixelSize) * prevGridScale - (width * pixelSize) * gridScale) / 2;
+    containerPosition.y += ((height * pixelSize) * prevGridScale - (height * pixelSize) * gridScale) / 2;
+    container.position.set(containerPosition.x, containerPosition.y);
+    animationFrameId = requestAnimationFrame(animateResize);
+  } else {
+    animationFrameId = null;
+  }
+};
+
 defineExpose({ randomGrid, clearGrid, stopGame, startGame });
 
 onMounted(async () => {
@@ -96,9 +139,9 @@ onMounted(async () => {
   pixiContainer.value.appendChild(app.canvas);
 
   app.stage.addChild(container);
-  container.scale.set(props.scale, props.scale);
+  container.scale.set(gridScale, gridScale);
   container.eventMode = 'static';
-
+  let firstMessage = false
   socket.onmessage = (e) => {
     const data = JSON.parse(e.data);
     if (height != data.gameState.height || width != data.gameState.width) {
@@ -116,14 +159,19 @@ onMounted(async () => {
       bg.height = height * pixelSize
       container.addChildAt(containerGrid, 0);
       container.addChildAt(bg, 1);
-      container.position.set(app.screen.width / 2 - ((width * pixelSize) * props.scale) / 2, app.screen.height / 2 - ((height * pixelSize) * props.scale) / 2);
+      containerPosition = new Point(app.screen.width / 2 - ((width * pixelSize) * gridScale) / 2, app.screen.height / 2 - ((height * pixelSize) * gridScale) / 2);
+      container.position.set(containerPosition.x, containerPosition.y);
     }
     if (data.gameState.playing) {
       emit('update:gameRunning', true);
     } else {
       emit('update:gameRunning', false);
     }
-    renderCells(data.cells);
+    if (!firstMessage) {
+      firstMessage = true
+      requestAnimationFrame(renderCells)
+    }
+    updateCells(data.cells);
   };
 
   socket.onclose = () => {
@@ -137,8 +185,9 @@ onMounted(async () => {
   let isDragging = false;
   container.on('pointerdown', (e) => {
     isDragging = true;
-    const x = Math.floor((e.global.x - container.position.x) / (pixelSize * props.scale));
-    const y = Math.floor((e.global.y - container.position.y) / (pixelSize * props.scale));
+    if (e.altKey) return;
+    const x = Math.floor((e.global.x - container.position.x) / (pixelSize * gridScale));
+    const y = Math.floor((e.global.y - container.position.y) / (pixelSize * gridScale));
     socket.send(JSON.stringify({ "x": x, "y": y }));
   });
 
@@ -147,15 +196,66 @@ onMounted(async () => {
   });
 
   container.on('pointermove', (e) => {
-    if (isDragging) {
-      const x = Math.floor((e.global.x - container.position.x) / (pixelSize * props.scale));
-      const y = Math.floor((e.global.y - container.position.y) / (pixelSize * props.scale));
+    if (isDragging && !e.altKey) {
+      const x = Math.floor((e.global.x - container.position.x) / (pixelSize * gridScale));
+      const y = Math.floor((e.global.y - container.position.y) / (pixelSize * gridScale));
       socket.send(JSON.stringify({ "x": x, "y": y }));
+    }
+  });
+
+  addEventListener('wheel', (e) => {
+    if (e.deltaY < 0) {
+      targetPixelSize = gridScale + 0.1;
+      if (targetPixelSize > 4) {
+        targetPixelSize = 4;
+      }
+    } else {
+      targetPixelSize = gridScale - 0.1;
+      if (targetPixelSize < 1) {
+        targetPixelSize = 1;
+      }
+    }
+
+    if (!animationFrameId) {
+      animationFrameId = requestAnimationFrame(animateResize);
+    }
+  });
+
+  addEventListener('resize', () => {
+    containerPosition = new Point(app.screen.width / 2 - ((width * pixelSize) * gridScale) / 2, app.screen.height / 2 - ((height * pixelSize) * gridScale) / 2);
+    container.position.set(containerPosition.x, containerPosition.y);
+  });
+
+  addEventListener('keydown', (e) => {
+    if (e.key === 'r') {
+      randomGrid();
+    } else if (e.key === 'c') {
+      clearGrid();
+    } else if (e.key === 's') {
+      if (props.gameRunning) {
+        stopGame();
+      } else {
+        startGame();
+      }
+    }
+  });
+
+  addEventListener('mousemove', (e) => {
+    if (e.altKey && isDragging) {
+      containerPosition.x += e.movementX
+      containerPosition.y += e.movementY
+      container.position.set(containerPosition.x, containerPosition.y)
     }
   });
 });
 
 onUnmounted(() => {
   app.destroy();
+  removeEventListener('wheel');
+  cancelAnimationFrame(animationFrameId);
+  removeEventListener('resize');
+  removeEventListener('keydown');
+  removeEventListener('mousemove');
+  removeEventListener('onclick');
 });
 </script>
